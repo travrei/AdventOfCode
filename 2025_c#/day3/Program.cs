da9cf6da75243c2328c1301c3dc86c4ab458507c
Andrei Esteves <qjogos7@gmail.com>
2025-12-04 12:32:06 -0300

    Adding comments to the part 1.

--- 2025_c#/day3/Program.cs
+++ 2025_c#/day3/Program.cs
@@ -1,34 +1,35 @@
 ﻿using System;
 using System.IO;
 
+//Getting the input file (I'm using the full path because of... convenience.)
 string filePath = "/home/andreie/dev/adventofcode/2025_c#/day3/input.txt";
 
 int totalVoltage = 0;
 
 try
 {
-    string[] lines = File.ReadAllLines(filePath);
-    foreach (string line in lines)
+    //Getting all the lines of the file
+    string[] lines = File.ReadAllLines(filePath); 
+    foreach (string line in lines) //Reading line by line
     {
-        int bateriesCount = line.Length;
+        int bateriesCount = line.Length; //Getting the total of batteries in my line
         int biggerVoltageOfTheLine = 0;
         for (int i = 0; i < bateriesCount - 1; i++)
         {
-            char cDozen = line[i];
-            
+            char cDozen = line[i]; //Grabbing the higher number to be the dozen
             for (int j = i + 1; j < bateriesCount; j++)
             {
-                char cUnit = line[j];
-                string voltageStr = cDozen.ToString() + cUnit.ToString();
+                char cUnit = line[j]; //Grabbing the next number to be the unit
+                string voltageStr = cDozen.ToString() + cUnit.ToString(); //Making the number
                 int voltage =  int.Parse(voltageStr);
-                if (voltage > biggerVoltageOfTheLine)
+                if (voltage > biggerVoltageOfTheLine) //Checking if the number is bigger than the last
                 {
                     biggerVoltageOfTheLine = voltage;
                 }
             }
         }
 
-        totalVoltage += biggerVoltageOfTheLine;
+        totalVoltage += biggerVoltageOfTheLine; //Sum the bigger number of the line in the total
     }
     Console.WriteLine($"Total: {totalVoltage}");
 }
